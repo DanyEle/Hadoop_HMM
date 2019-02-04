10c376ba9afa2bce12fdb112af33f17f87473038
Daniele Gadler <daniele.gadler@yahoo.it>
2019-02-04 16:55:57 +0100

    Serialization and communication between mapper and reducer completed

---
+++ HMM_MapReduce/run_program.sh
@@ -0,0 +1,5 @@
+mvn clean package
+hadoop jar target/hmm-0.0.1-SNAPSHOT.jar it.cnr.isti.pad.HMM input output
+rm -r output
+hadoop fs -get output ./
+ls output
--- HMM_MapReduce/src/main/java/it/cnr/isti/pad/HMM.java
+++ HMM_MapReduce/src/main/java/it/cnr/isti/pad/HMM.java
@@ -54,12 +54,10 @@ public class HMM
 			System.out.println("Amount of messages before removing outliers: " + logFile.messages.size());
 			//remove all those lines that contain an outlier message.
 			logFile = Utilities.removeOutliers(logFile, messagesRemove);
-			System.out.println("Amount of messages after removing outliers: " + logFile.messages.size());
-			//now group messages based on their sequence IDs.
-			seqIDStart = Utilities.extraSeqIDFromFilePath(filePath);
-			//EX: Sequence ID 31 --> 31.000.000. This is done to avoid having sequenceIDs overlapping
-			//from different files
-			seqIDStart *= seqIDMultiplier;			
+			System.out.println("Amount of messages after removing outliers: " + logFile.messages.size());			//now group messages based on their sequence IDs.
+			seqIDStart = Utilities.extraSeqIDFromFilePath(filePath) * seqIDMultiplier;
+			//EX: Sequence ID 31 --> 31.000.000. This is done to avoid having 
+			//sequenceIDs from different files overlapping
 			logFile = Utilities.markSequenceIDs(logFile, seqIDStart);
 			System.out.println("Amount of messages after marking sequence IDs and removing messages not in sequences): " + logFile.sequenceIDs.size());
 			//now remove all the sequences that are too small (< 2 minutes in length) and large sessions (> 100 minutes in length)
@@ -79,17 +77,20 @@ public class HMM
 	//Input: - Text key: the (unique) path of the file being parsed
 	//		 - LogFile value: the input logFile received
 	//Output: - Text key: the filename 
-	//		  - value: the amount of sequences found in all files
+	//		  - value: logFile corresponding to this key
 	//dummy reducer for now.
-	public static class NewReducer extends Reducer<Text, LogFile, Text, IntWritable>
+	public static class NewReducer extends Reducer<Text, LogFile, Text, LogFile>
 	{
 		//Output the total amount of sequences identified. 
-		private IntWritable result = new IntWritable();
-
 		public void reduce(Text key, Iterable<LogFile> logFiles, Context context) throws IOException, InterruptedException
 		{
-			result.set(0);
-			context.write(key, result);
+			System.out.println("Key received" + key);
+			
+			for(LogFile logFile : logFiles)
+			{
+				context.write(key, logFile);
+			}
+			
 		}
 	}
 
@@ -113,28 +114,24 @@ public class HMM
 		job.setJarByClass(HMM.class);
 		
 		//need to process whole file, not just single line here.
-		
 		//Mapper output values
 		job.setMapOutputKeyClass(Text.class);
 		job.setMapOutputValueClass(LogFile.class);
+		job.setInputFormatClass(WholeFileInputFormat.class);
+
 		//Reduce output values
 		job.setOutputKeyClass(Text.class);
 		job.setOutputValueClass(IntWritable.class);
 		
 		//just for testing purposes, try merging with one single reducer
-		job.setNumReduceTasks(1);
+		//job.setNumReduceTasks(1);
+
 
 		job.setMapperClass(NewMapper.class);
 		job.setReducerClass(NewReducer.class);
 
 		FileInputFormat.addInputPath(job, new Path(args[0]));
-		FileOutputFormat.setOutputPath(job, new Path(args[1]));
-		
-		job.setInputFormatClass(WholeFileInputFormat.class);
-
-		
-
-		
+		FileOutputFormat.setOutputPath(job, new Path(args[1]));		
 		
 
 		System.exit(job.waitForCompletion(true) ? 0 : 1);
